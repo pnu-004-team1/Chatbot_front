e0b5c651fd2a4f8cb32f2c2e256bcd62262815d2
Kooku <koomg9599@gmail.com>
2019-04-25 18:10:04 +0900

    css , input 수정

--- src/App.js
+++ src/App.js
@@ -5,35 +5,65 @@ class App extends Component {
   id = 3;
   state = {
     input: '',
-    message: [
-      { id: 0, text: ' 안녕', checked: false },
-      { id: 1, text: ' 하이2', checked: true },
-      { id: 2, text: ' 헬루', checked: false }
+    messages: [
+      { id: 0, text: ' 안녕', from: 'me' },
+      { id: 1, text: ' 하이2', from: 'me' },
+      { id: 2, text: ' 헬루', from: 'other' },
     ]
   }
 
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter'){
+      this.handleCreate();
+    }
+      
+  }
+  handleCreate = () => {
+    const { input, messages } = this.state;
+    this.setState({
+      input: '', // 인풋 비우고 concat을 사용하여 배열에 추가
+      messages: messages.concat({
+        id: this.id++,
+        text: input,
+        from: 'me'
+      })
+    });
+  }
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value // input 의 다음 바뀔 값
+    });
+  }
   render() {
-    const { input, message } = this.state;
-    const messageList = message.map(
-      ({id, text, checked}) => (
-        <div className="me">
-          {text}  
-        </div>
+    const { input, messages } = this.state
+    const { handleKeyPress, handleChange } = this
+    const messageList = messages.map(
+      ({id, text, from}) => (
+        <p className={from}>
+          {text}
+        </p>
       )
     )
     
     return (
-      <div>
-        <div id="main">
-          <div id="chat">
+      <React.Fragment>
+        <div className="main">
+          <div className="chat">
             {messageList}
           </div>
-          <div>
-            <input type="text" id="test" placeholder="message.." onkeypress="JavaScript:press()"/>
+          <div className="form">
+            <input
+              type="text"
+              id="test"
+              placeholder="message.."
+              value={input}
+              onKeyPress={handleKeyPress}
+              onChange={handleChange}
+            />
             <button onclick="send()">Submit</button>
           </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
