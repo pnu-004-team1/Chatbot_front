8dff42425a5f3ab75382be7d019764402c29de5b
Kooku <koomg9599@gmail.com>
2019-05-02 20:04:25 +0900

    floating action button 구현

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,14 +1,50 @@
 <template>
-    <Chat/>
+  <div>
+    <iframe
+      id="site-frame"
+      src="http://www.pusan.ac.kr/kor/Main.do"
+      frameborder="0"
+      width="100%"
+      height="1500"
+      sandbox="allow-same-origin allow-scripts"
+    />
+    <fab
+      :position="position"
+      :bg-color="bgColor"
+      :actions="fabActions"
+      @chatbot="chat"
+    />
+    <Chat v-show="this.chatFlag"/>
+  </div>
 </template>
 
 <script>
 import Chat from './chat/Chat'
+import fab from 'vue-fab'
 
 export default {
   name: 'Home',
   components: {
-    'Chat': Chat
+    'Chat': Chat,
+    'fab': fab
+  },
+  data () {
+    return {
+      bgColor: '#778899',
+      position: 'bottom-right',
+      chatFlag: false,
+      fabActions: [
+        {
+          name: 'chatbot',
+          icon: 'question_answer'
+        }
+      ]
+    }
+  },
+  methods: {
+    chat () {
+      this.chatFlag = !this.chatFlag
+    }
   }
 }
 </script>
--- src/components/chat/Chat.vue
+++ src/components/chat/Chat.vue
@@ -2,11 +2,11 @@
     <div class="container">
         <!-- BEGIN : chatHeader -->
         <div class="chatHeader">
-        <a href="#" class="BackBtn"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
-        <BuggerMenu/>
+          <a href="#" class="BackBtn"><i class="fa fa-angle-left" aria-hidden="true"></i></a>
+          <BuggerMenu/>
         <div class="headerTitle">
             <!-- <div class="NameIcon">SA</div> -->
-            <img class="NameIcon" src="../../common/images/logo.png"/>
+          <img class="NameIcon" src="../../common/images/logo.png"/>
             <div class="Txt">
               <div>PNU Chatbot</div>
               <div>무엇이든 물어보세요!</div>
@@ -16,24 +16,24 @@
         <!-- END : chatHeader -->
         <!-- BEGIN : chatContent -->
         <div class="chatContent">
-            <Message
-                v-for="(item) in getMessages"
-                v-bind:item="item"
-                v-bind:key="item.id"
-            />
+          <Message
+              v-for="(item) in getMessages"
+              v-bind:item="item"
+              v-bind:key="item.id"
+          />
         </div>
         <!-- END : chatContent -->
         <div class="chatSend">
-        <input
-            type="text"
-            class="sendText"
-            placeholder="message.."
-            v-model="input"
-            v-on:keydown="handelPress"
-        />
-        <div id="send-message" onClick={handleCreate}>
+          <input
+              type="text"
+              class="sendText"
+              placeholder="message.."
+              v-model="input"
+              v-on:keydown="handelPress"
+          />
+          <div id="send-message" onClick={handleCreate}>
             <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-        </div>
+          </div>
         </div>
     </div>
 </template>
