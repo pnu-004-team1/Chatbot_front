f814c28e3071aa4e5b6db737c8617a4f71fd6467
Kooku <koomg9599@gmail.com>
2019-06-01 23:48:11 +0900

    메세지 에러, iframe 위치에러 해결, x 생성으로 채팅창 open&close

--- src/common/string.js
+++ src/common/string.js
@@ -3,3 +3,7 @@ export const LOGIN = 'login'
 export const LOGOUT = 'logout'
 export const SIGNIN = 'signin'
 export const SIGNUP = 'signup'
+export const CLOSE = ''
+
+export const PC = 'pc'
+export const PHONE = 'phone'
--- src/store/modules/home.js
+++ src/store/modules/home.js
@@ -4,16 +4,36 @@ Vue.use(Vuex)
 
 export const home = {
   state: {
-    chatComponentStatus: ''
+    chatComponentStatus: '',
+    containerVisible: false,
+    environment: 'pc'
   },
   getters: {
     getChatComponentStatus: (state) => {
       return state.chatComponentStatus
+    },
+    getContainerVisible: (state) => {
+      return state.containerVisible
+    },
+    getEnvironment: (state) => {
+      return state.environment
     }
   },
   mutations: {
     updateChatComponentStatus: (state, payload) => {
       state.chatComponentStatus = payload
+    },
+    containerDisplay: (state) => {
+      state.containerVisible = true
+    },
+    containerHidden: (state) => {
+      state.containerVisible = false
+    },
+    toggleContainerVisible: (state) => {
+      state.containerVisible = !state.containerVisible
+    },
+    setEnvironment: (state, payload) => {
+      state.environment = payload
     }
   },
   actions: {
